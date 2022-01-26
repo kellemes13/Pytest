4544d3ee99b5c570dc74f3a1d69ac1cc3804f2de
lilly <kellemes13@gmail.com>
2022-01-26 10:54:57 +0800

    add time and progress bar

--- Review.py
+++ Review.py
@@ -1,18 +1,25 @@
 import os
+import time
+import progressbar
+
 data=[]
 count=0
 countt=0
+bar = progressbar.ProgressBar(max_value=1000000)
+
 #calcaulate the line of review
 
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count+=1
+		bar.update(count)
 		#if count%1000==0:
 			#print(len(data))
 #count=0
 print('total line: ',len(data))
 
+start = time.time()
 word_count={}
 for d in data:
 	words= d.split()
@@ -27,6 +34,9 @@ for d in data:
 for index in word_count:
 	if word_count[index] >1000000:
 		print(index,word_count[index])
+end =time.time()
+
+print('using ',(end-start),' seconds')
 
 while 1:
 	word = input('please enter the word :')
